4ea64f1e94ca5913b2cef0faad9b16b5f96cc6b5
Karen Celis <k-celis@javeriana.edu.co>
2019-10-21 01:25:11 -0500

    Creación de Actividades

---
+++ app/src/main/java/com/example/proyecto/VistaArchivoMultimedia.java
@@ -0,0 +1,14 @@
+package com.example.proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VistaArchivoMultimedia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_archivo_multimedia);
+    }
+}
