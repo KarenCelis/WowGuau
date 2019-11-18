c71888acfd471ee66e2d2365517747afd5c7bddd
Lina Marcela Varón <31346699+linamvaron97@users.noreply.github.com>
2019-11-17 21:32:38 -0500

    Update ListaPaseadoresCercanos.java

--- WowGuauV2/app/src/main/java/com/example/wowguauv2/ListaPaseadoresCercanos.java
+++ WowGuauV2/app/src/main/java/com/example/wowguauv2/ListaPaseadoresCercanos.java
@@ -1,13 +1,19 @@
 package com.example.wowguauv2;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -21,48 +27,92 @@ import java.util.List;
 public class ListaPaseadoresCercanos extends AppCompatActivity {
 
     public static final String PATH_PASEADOR = "user/paseador/";
+    public static final int LOCATION = 7;
+    double RADIUS_OF_EARTH_KM = 6371;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference(PATH_PASEADOR);
     ArrayList<String> distPaseador = new ArrayList<>();
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lista_paseadores_cercanos);
 
-        loadUsers(myRef);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, distPaseador);
+
+        setContentView(R.layout.activity_lista_paseadores_cercanos);
         ListView list_paseador = findViewById(R.id.listpaseador);
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, distPaseador);
         list_paseador.setAdapter(adapter);
+
+
+        askPermission();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            Toast.makeText(this, "Permiso de localización denegado!", Toast.LENGTH_LONG).show();
+
+        } else {
+
+            Toast.makeText(this, "Permiso de localización aceptado!", Toast.LENGTH_LONG).show();
+            loadUsers(myRef,distPaseador);
+        }
     }
 
 
-    public void loadUsers(DatabaseReference myRef) {
+    public void loadUsers(DatabaseReference myRef, final ArrayList distPaseador) {
 
-        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+        myRef.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
 
-                for(DataSnapshot singleSnapshot : dataSnapshot.getChildren()) {
+                Usuario paseador = dataSnapshot.getValue(Usuario.class);
+                String nombre = paseador.getNombre();
+                distPaseador.add(nombre);
+                adapter.notifyDataSetChanged();
+            }
 
-                    Usuario paseador = singleSnapshot.getValue(Usuario.class);
-                    String nombrePas = paseador.getNombre();
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            }
 
-                    for(int i = 0; i < distPaseador.size(); i++){
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+            }
 
-                        distPaseador.add("Hola");
-                    }
-
-                    Toast.makeText(ListaPaseadoresCercanos.this, nombrePas, Toast.LENGTH_LONG).show();
-                }
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                Toast.makeText(ListaPaseadoresCercanos.this,"ERROOOOR", Toast.LENGTH_LONG).show();
             }
         });
 
     }
+
+    private void askPermission() {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Show an expanation to the user *asynchronouslyÂ  Â
+                Toast.makeText(this, "Se necesita el permiso para ver los paseadores!", Toast.LENGTH_LONG).show();
+            }
+            // Request the permission.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION);
+        }
+    }
+
+    public double distance(double lat1, double long1, double lat2, double long2) {
+
+        double latDistance = Math.toRadians(lat1 - lat2);
+        double lngDistance = Math.toRadians(long1 - long2);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lngDistance / 2) * Math.sin(lngDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double result = RADIUS_OF_EARTH_KM * c;
+        return Math.round(result*100.0)/100.0;
+    }
+
 }
