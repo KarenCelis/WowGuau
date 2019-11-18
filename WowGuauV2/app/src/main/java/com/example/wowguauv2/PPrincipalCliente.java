ee540f40a07e8b28d31519203246c60fc589aec5
Karen Celis <k-celis@javeriana.edu.co>
2019-11-18 13:08:24 -0500

    Creacion de clase paseos en curso

--- WowGuauV2/app/src/main/java/com/example/wowguauv2/PPrincipalCliente.java
+++ WowGuauV2/app/src/main/java/com/example/wowguauv2/PPrincipalCliente.java
@@ -1,9 +1,12 @@
 package com.example.wowguauv2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -61,5 +64,21 @@ Button cerrar;
             }
         });
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_layout, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId()==R.id.signOutMenuItd){
+
+            FirebaseAuth.getInstance().signOut();
+            finish();
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
