d18e40c9713e7d2b5cd5cf3b1d9d519dff054ef2
Karen Celis <k-celis@javeriana.edu.co>
2019-11-19 20:05:02 -0500

    Update VerMapaPaseoEnCurso.java

--- WowGuauV2/app/src/main/java/com/example/wowguauv2/VerMapaPaseoEnCurso.java
+++ WowGuauV2/app/src/main/java/com/example/wowguauv2/VerMapaPaseoEnCurso.java
@@ -1,11 +1,13 @@
 package com.example.wowguauv2;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -16,11 +18,24 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class VerMapaPaseoEnCurso extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+
+    private Marker oldmark;
+    private Marker newmark;
+    private Marker lastmark;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +45,35 @@ public class VerMapaPaseoEnCurso extends FragmentActivity implements OnMapReadyC
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        database= FirebaseDatabase.getInstance();
+        myRef = database.getReference("/user/paseador/obnrSf46A7RjMq7p2wmODj8cPo23/");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()) {
+                    Log.i("koko", "Encontró usuario: " + dataSnapshot.toString());
+                    Paseador myUser = dataSnapshot.getValue(Paseador.class);
+                    Log.i("koko", "Encontró usuario: " + myUser.toString());
+                    String name = myUser.getNombre();
+                    Double lati = myUser.getLatitud();
+                    Double longi = myUser.getLongitud();
+                    String msj = ""+ lati + "," + longi;
+                    Toast.makeText(VerMapaPaseoEnCurso.this, name + ":" + msj, Toast.LENGTH_SHORT).show();
+
+                    LatLng walker = new LatLng(lati, longi);
+                    oldmark.remove();
+                    newmark = mMap.addMarker(new MarkerOptions().position(walker).title("Paseador"));
+                    oldmark = newmark;
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(walker));
+                //}
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("koko", "error en la consulta", databaseError.toException());
+            }
+        });
     }
 
 
@@ -47,8 +91,8 @@ public class VerMapaPaseoEnCurso extends FragmentActivity implements OnMapReadyC
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng bog = new LatLng(4, -74);
+        oldmark = mMap.addMarker(new MarkerOptions().position(bog).title("Bog"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(bog));
     }
 }
